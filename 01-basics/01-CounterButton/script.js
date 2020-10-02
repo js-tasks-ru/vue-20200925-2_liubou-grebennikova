f43424afdee0ec4d12962480f52219a2f27b4f36
elle <liubou.grebennikova@gmail.com>
2020-10-02 08:52:48 +0300

    Counter button created

--- 01-basics/01-CounterButton/script.js
+++ 01-basics/01-CounterButton/script.js
@@ -2,3 +2,15 @@ import Vue from './vue.esm.browser.js';
 
 // const app = ...
 // Рекомендуется использовать МЕТОД в качестве обработчика события
+
+const app = new Vue({
+    el: '#app',
+    data: {
+      btnText: 0
+    },
+    methods: {
+        increaseByOne () {
+          return this.btnText ++
+      }
+    }
+  })
