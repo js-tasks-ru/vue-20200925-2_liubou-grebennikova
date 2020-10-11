a8c58dfa101b7a21170c9b3cb430c63a40ee4504
elle <liubou.grebennikova@gmail.com>
2020-10-11 14:07:35 +0300

    CounterButtonComponent task is solved

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,10 +1,29 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: '<button type="button"@click="onClick">{{count}}</button>',
+  props: {
+    count: {
+      type: Number,
+      required: false,
+      default: 0,
+      validator: value => typeof value === 'number',
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit('increment', this.count + 1)
+    }
+  },
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+
 
   // Компонент должен иметь входной параметр
 
   // Компонент должен иметь модель
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+
 };
